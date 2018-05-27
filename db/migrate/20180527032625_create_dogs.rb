561a47931a4ff866589ace90472cdeb4a498d465
Veronica Kim <vero.a.kim@gmail.com>
2018-05-27 03:27:39 +0000

    automatically backed up by learn

--- db/migrate/20180527032625_create_dogs.rb
+++ db/migrate/20180527032625_create_dogs.rb
@@ -1,7 +1,11 @@
 class CreateDogs < ActiveRecord::Migration
   def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
   end
 
   def down
-  end 
+    drop_table :dogs 
+  end
 end
